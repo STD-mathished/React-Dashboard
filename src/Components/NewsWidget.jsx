8265dcb5a04e10f78bc2c822e7f366f7686b3c2e
Mathis Heder <mathis.heder@universite-paris-saclay.fr>
2025-07-31 14:53:47 +0200

    News pannel connected

--- src/Components/NewsWidget.jsx
+++ src/Components/NewsWidget.jsx
@@ -1,19 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function NewsWidget() {
-  const headlines = [
-    "Global Markets Rally as Investors Digest Economic Data",
-    "New Study Reveals the Benefits of a Plant-Based Diet",
-    "Tech Companies Unveil Latest Advancements in AI",
-    "Sports Update: Local Team Claims Victory in Championship"
-  ];
+  const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(data.articles);
+      })
+      .catch((error) => {
+        console.error("Erreur API News:", error)
+      });
+  }, []);
 
   return (
     <div className="rounded-2xl shadow p-4 bg-white">
       <h2 className="text-lg font-bold mb-2">News</h2>
-      <ul className="list-disc pl-5 text-sm">
-        {headlines.map((headline, index) => (
-          <li key={index} className="mb-1">{headline}</li>
+      <ul className="text-sm list-disc pl-5">
+        {articles.map((article, index) => (
+          <li key={index} className="mb-2">
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className=" hover:underline">
+              {article.title}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
   );
-};
+}
